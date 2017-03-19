0b408ffbc94f07ba7513744a02ae7326bf06d179
Dzmitry Dubarau <ddzmitry@yahoo.com>
2017-03-19 01:01:56 -0400

    Fixed Animal's API to get needed info

---
+++ petFinderTest.js
@@ -0,0 +1,26 @@
+var petfinder = require('petfinder')('357d4a946c3d94988341843dbe6abed5', '6b28ab1dc3723180ab6a01aa0491b5fe');
+
+// Get list of breeds 
+// petfinder.getBreedList('cat', function(err, breeds) {
+//   console.log(breeds)
+// });
+
+
+petfinder.findPet('28105', { animal: 'cat', age: 'Adult', size: 'M' }, function(err, breeds) {
+    for (var i = 0; i < breeds.length; i++) {
+        console.log(breeds[i].sex)
+        console.log(breeds[i].age)
+
+        console.log(breeds[i].description)
+        console.log(breeds[i].shelterId)
+            //Here we need to do fnction if objHasAkey(for phone email and address and if it does then display it)
+        console.log('_______CONTACT INFO_________')
+        console.log(breeds[i].contact)
+        console.log(breeds[i].options)
+        console.log(`_______Picture ${i}___________`)
+        console.log(breeds[i].media.photos['1'].x)
+        console.log(`_______NEXT PET ${i}___________`)
+    }
+
+
+});
