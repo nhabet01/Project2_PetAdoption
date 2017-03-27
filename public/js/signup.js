816d6475047a4735025124315ab2122e81bde04a
Dzmitry Dubarau <ddzmitry@yahoo.com>
2017-03-27 16:34:22 -0400

    validation front end

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,13 +1,86 @@
 $(document).ready(function() {
-    console.log("ready!");
+    // console.log("ready!");
+    let passwordArr = []
+    let passwordtoMatch = []
+    let emailtomatch = []
+    let email;
+    var name = $('#name');
+    var username = $('#username');
 
 
-    console.log($('#password').val())
+    function isEmail(email) {
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
+    }
+    $('#email').keyup((e) => {
+
+        // console.log(e.originalEvent.key)
 
 
-    $('#confirm').keyup(function(e) {
+        if (e.originalEvent.key == 'Backspace') {
+            emailtomatch.pop()
+        } else if (e.originalEvent.key == 'Tab' || e.originalEvent.key == 'Shift') {
 
-        console.log(e)
+        } else { emailtomatch.push(e.originalEvent.key) }
+        // console.log(emailtomatch)
+        email = emailtomatch.join('')
+            // console.log(email)
+            // console.log(name.val().length)
     });
+
+
+
+
+    $('.btn').prop('disabled', true);
+    $('#password').keyup((e) => {
+
+        // console.log(e.originalEvent.key)
+
+
+        if (e.originalEvent.key == 'Backspace') {
+            passwordArr.pop()
+        } else if (e.originalEvent.key == 'Tab') {
+
+        } else { passwordArr.push(e.originalEvent.key) }
+        // console.log(passwordArr, passwordtoMatch)
+    });
+
+    $('#confirm').keyup((e) => {
+
+        // console.log(e.originalEvent.key)
+
+        if (e.originalEvent.key == 'Backspace') {
+            passwordtoMatch.pop()
+        } else if (e.originalEvent.key == 'Tab') {
+
+        } else {
+            passwordtoMatch.push(e.originalEvent.key)
+        }
+        console.log(passwordArr, passwordtoMatch)
+        Confurm(email, name, username)
+    });
+
+    let Confurm = (email, name, username) => {
+        if (passwordArr.length >= 5 && passwordtoMatch.length >= 5 && name.val().length >= 4 && username.val().length >= 4) {
+            if (passwordArr.toString() == passwordtoMatch.toString()) {
+                console.log('password is matching')
+                    // console.log(email)
+                    // console.log(isEmail(email))
+
+                if (isEmail(email)) {
+                    $('.btn').prop('disabled', false);
+                }
+
+            } else {
+                console.log('password is not matching')
+
+            }
+
+        } else {
+
+        }
+
+    }
+
 
 });
